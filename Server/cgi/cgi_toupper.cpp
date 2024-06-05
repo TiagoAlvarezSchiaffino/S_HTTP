a1750ab372dae5f50f32bc1359207b21862dfca6
TiagoAlvarezSchiaffino <t.alvarezschiaffino@gmail.com>
2024-06-05 11:48:30 -0300

    Fix crash bug on toupper cgi

--- Server/cgi/cgi_toupper.cpp
+++ Server/cgi/cgi_toupper.cpp
@@ -8,7 +8,7 @@
 /*                                                            (    @\___      */
 /*                                                             /         O    */
 /*   Created: 2024/06/05 10:35:10 by Tiago                    /   (_____/     */
-/*   Updated: 2024/06/05 10:37:55 by Tiago                  /_____/ U         */
+/*   Updated: 2024/06/05 11:48:31 by Tiago                  /_____/ U         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,15 +22,13 @@
 
 int    main(void)
 {
-    // char    buffer[30000000];
-
-    // std::string buffer(10000000, '\0');
-    std::string buffer(WS_BUFFER_SIZE, '\0');
-    // buffer.resize(1000000);
-    read(STDIN_FILENO, &buffer[0], WS_BUFFER_SIZE);
-    // std::cerr << "Buffer: " << buffer <<std::endl;
+    char	*buffer = new char[WS_BUFFER_SIZE];
+	std::cin.getline(buffer, WS_BUFFER_SIZE);
 
     for (size_t i = 0; buffer[i]; i++)
         buffer[i] = toupper(buffer[i]);
     std::cout << buffer;
+
+    	delete[] buffer;
+    return (0);
 }
