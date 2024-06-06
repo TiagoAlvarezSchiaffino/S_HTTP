8f79505169ff7cae997e3bf1b9094bb20912b964
TiagoAlvarezSchiaffino <t.alvarezschiaffino@gmail.com>
2024-06-06 05:26:05 -0300

    Implement cookie page

--- Server/srcs/EuleeHand.cpp
+++ Server/srcs/EuleeHand.cpp
@@ -8,19 +8,23 @@
 /*                                                            (    @\___      */
 /*                                                             /         O    */
 /*   Created: 2024/06/03 14:20:49 by Tiago                    /   (_____/     */
-/*   Updated: 2024/06/06 05:04:26 by Tiago                  /_____/ U         */
+/*   Updated: 2024/06/06 05:26:35 by Tiago                  /_____/ U         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "EuleeHand.hpp"
 
-EuleeHand::EuleeHand() : envp(), cgi(), statusList(), buffer(), server(), serverFd(), serverAddr(), socket(), _envpSize() {}
+EuleeHand::EuleeHand() : envp(), cgi(), statusList(), buffer(), server(), serverFd(), serverAddr(), socket(), connectionCount(), cookieJar(), _envpSize() {}
 
-EuleeHand::EuleeHand(std::string configFilePath, const ConfigManager &configManager, char **envp) : envp(), cgi(), statusList(), buffer(), server(), serverFd(), serverAddr(), socket(), _envpSize(), _configFilePath(configFilePath), _configManager(configManager)
+EuleeHand::EuleeHand(std::string configFilePath, const ConfigManager &configManager) : envp(), cgi(), statusList(), buffer(), server(), serverFd(), serverAddr(), socket(), connectionCount(), cookieJar(), _envpSize(), _configFilePath(configFilePath), _configManager(configManager)
 {
-	this->envp = new char*[100];
-	for (size_t i = 0; envp[i]; ++i)
-		this->addEnv(envp[i]);
+	this->envp = new char*[16];
+	for (size_t i = 0; i < 15; ++i)
+	{
+		this->envp[i] = new char[1000];
+		std::memset(this->envp[i], 0, 1000);
+	}
+	this->envp[16] = NULL;
 }
 
 EuleeHand::~EuleeHand() {}
@@ -248,9 +252,10 @@ std::string EuleeHand::_getFileCreationTime(const std::string &path, const std::
 	int rv = stat((full_path).c_str(), &result);
 	if (rv == 0)
 	{
-    	std::time_t last_modified_time = result.st_mtime;   
-    	std::time_t modified_time = static_cast<std::time_t>(last_modified_time);
-		return (std::ctime(&modified_time));
+		// joel - wsl (removed std from std::time_t and std::ctime)
+    	time_t last_modified_time = result.st_mtime;   
+    	time_t modified_time = static_cast<time_t>(last_modified_time);
+		return (ctime(&modified_time));
     }
 	else if (rv == -1)
     	std::cerr << "File {" <<  full_path << "} not found." << std::endl;
@@ -342,7 +347,9 @@ int	EuleeHand::checkPath(std::string path, int isFile, int isDirectory)
 {
     std::ifstream   temp(path + "/");
     if (temp.good() && isFile == 1 && isDirectory == 0)
-        return (0);
+	{
+    	return (0);
+	}
 	std::ifstream	file(path);
 	if (file.good()) // is a directory and a file
 	{
@@ -391,12 +398,12 @@ int	EuleeHand::checkExcept()
 	return (0);
 }
 
-size_t	EuleeHand::_readFile(std::string *buffer1, std::string *buffer2, int infile, char *temp, long bytes_read, int type, int *count)
+size_t	EuleeHand::_readFile(std::string *buffer1, std::string *buffer2, int infile, char *temp, long bytesRead, int type, int *count)
 {
 	size_t	current_size = 0;
 
 	if (*count > type)
-		buffer2->append(temp, bytes_read);
+		buffer2->append(temp, bytesRead);
 	else
 		{
 		if (buffer1->find("\r\n") != buffer1->length() - 2)
@@ -407,8 +414,8 @@ size_t	EuleeHand::_readFile(std::string *buffer1, std::string *buffer2, int infi
 			{
 				*buffer1 += str;
 				std::memset(temp, 0, WS_BUFFER_SIZE);
-				bytes_read = read(infile, temp, WS_BUFFER_SIZE);
-				current_size += bytes_read;
+				bytesRead = read(infile, temp, WS_BUFFER_SIZE);
+				current_size += bytesRead;
 				std::string	next(temp);
 				str = next;
 				pos = str.find("\r\n");
@@ -426,7 +433,7 @@ size_t	EuleeHand::_readFile(std::string *buffer1, std::string *buffer2, int infi
 int	EuleeHand::_unchunkIntofile(int fd, std::string bufferIn, int isHeader)
 {
 	if (bufferIn == "")
-		return (1);
+		return (0);
 	size_t		pos = bufferIn.find("\r\n\r\n");
 	std::string	remaining = bufferIn;
 	if (isHeader)
@@ -449,15 +456,21 @@ int	EuleeHand::_unchunkIntofile(int fd, std::string bufferIn, int isHeader)
 		}
 		catch(const std::exception& e)
 		{
-			std::cout << "Chunk Error: Hex size is less than chunk size!" << std::endl;
+			std::cerr << RED << "Chunk Error: Hex size is less than chunk size!" << RESET << std::endl;
+			this->sendHttp(400);
+			return (1);
 		}
 		if (size == 0)
 			break ;
-		if (size > remaining.size() - std::strlen("\r\n"))
-			std::cout << "Chunk Error: Hex size is more than remaining size!" << std::endl;
-		std::string	tmp = remaining.substr(pos + std::strlen("\r\n"), size);
+		if (size > remaining.size() - strlen("\r\n"))
+		{
+			std::cerr << RED << "Chunk Error: Hex size is more than remaining size!" << RESET << std::endl;
+			this->sendHttp(400);
+			return (1);
+		}
+		std::string	tmp = remaining.substr(pos + strlen("\r\n"), size);
 		write(fd, tmp.c_str(), tmp.size());
-		remaining = remaining.substr(pos + size + std::strlen("\r\n\r\n"));
+		remaining = remaining.substr(pos + size + strlen("\r\n\r\n"));
 	}
 	return (0);
 }
@@ -467,56 +480,55 @@ int	EuleeHand::unchunkResponse()
 	if (this->buffer[this->socket].find("Transfer-Encoding: chunked") == std::string::npos)
 		return (0);
 	std::vector<std::string>	bufferVector(10, "");
-	long		bytes_read = 0;
-	int			infile = open(WS_UNCHUNK_INFILE, O_RDWR | O_CREAT | O_TRUNC, 0777);
+	long						bytesRead = 0;
+	int							infile = open(WS_UNCHUNK_INFILE, O_RDWR | O_CREAT | O_TRUNC, 0777);
 	write(infile, this->buffer[this->socket].c_str(), this->buffer[this->socket].size());
 	close(infile);
-
-	infile = open(WS_UNCHUNK_INFILE, O_RDONLY, 0777);
-	char		*temp = new char[WS_BUFFER_SIZE + 1];
-    std::memset(temp, 0, WS_BUFFER_SIZE + 1);
 
 	std::ifstream	countSize(WS_UNCHUNK_INFILE);
 	countSize.seekg(0, std::ios::end);
 	size_t	total = countSize.tellg();
 	countSize.seekg(0, std::ios::beg);
 	countSize.close();
-	size_t	current_size = 0;
+
+	size_t		current_size = 0;
+	char		*temp = new char[WS_BUFFER_SIZE + 1];
+    std::memset(temp, 0, WS_BUFFER_SIZE + 1);
 	infile = open(WS_UNCHUNK_INFILE, O_RDONLY, 0777);
 	if (total <= 25000000)
 	{
-		while ((bytes_read = read(infile, temp, WS_BUFFER_SIZE)) > 0)
+		while ((bytesRead = read(infile, temp, WS_BUFFER_SIZE)) > 0)
 		{
-			bufferVector[0].append(temp, bytes_read);
+			bufferVector[0].append(temp, bytesRead);
 			std::memset(temp, 0, WS_BUFFER_SIZE + 1);
 		}
 	}
 	else
 	{
 		int	count = 0;
-		while ((bytes_read = read(infile, temp, WS_BUFFER_SIZE)) > 0)
+		while ((bytesRead = read(infile, temp, WS_BUFFER_SIZE)) > 0)
 		{
-			current_size += bytes_read;
+			current_size += bytesRead;
 			if (current_size <= total / 10)
-				bufferVector[0].append(temp, bytes_read);
+				bufferVector[0].append(temp, bytesRead);
 			else if (current_size > total / 10 && current_size <= total * 2 / 10)
-				current_size += this->_readFile(&bufferVector[0], &bufferVector[1], infile, temp, bytes_read, 0, &count);
+				current_size += this->_readFile(&bufferVector[0], &bufferVector[1], infile, temp, bytesRead, 0, &count);
 			else if (current_size > total * 2 / 10 && current_size <= total * 3 / 10)
-				current_size += this->_readFile(&bufferVector[1], &bufferVector[2], infile, temp, bytes_read, 1, &count);
+				current_size += this->_readFile(&bufferVector[1], &bufferVector[2], infile, temp, bytesRead, 1, &count);
 			else if (current_size > total * 3 / 10 && current_size <= total * 4 / 10)
-				current_size += this->_readFile(&bufferVector[2], &bufferVector[3], infile, temp, bytes_read, 2, &count);
+				current_size += this->_readFile(&bufferVector[2], &bufferVector[3], infile, temp, bytesRead, 2, &count);
 			else if (current_size > total * 4 / 10 && current_size <= total * 5 / 10)
-				current_size += this->_readFile(&bufferVector[3], &bufferVector[4], infile, temp, bytes_read, 3, &count);
+				current_size += this->_readFile(&bufferVector[3], &bufferVector[4], infile, temp, bytesRead, 3, &count);
 			else if (current_size > total * 5 / 10 && current_size <= total * 6 / 10)
-				current_size += this->_readFile(&bufferVector[4], &bufferVector[5], infile, temp, bytes_read, 4, &count);
+				current_size += this->_readFile(&bufferVector[4], &bufferVector[5], infile, temp, bytesRead, 4, &count);
 			else if (current_size > total * 6 / 10 && current_size <= total * 7 / 10)
-				current_size += this->_readFile(&bufferVector[5], &bufferVector[6], infile, temp, bytes_read, 5, &count);
+				current_size += this->_readFile(&bufferVector[5], &bufferVector[6], infile, temp, bytesRead, 5, &count);
 			else if (current_size > total * 7 / 10 && current_size <= total * 8 / 10)
-				current_size += this->_readFile(&bufferVector[6], &bufferVector[7], infile, temp, bytes_read, 6, &count);
+				current_size += this->_readFile(&bufferVector[6], &bufferVector[7], infile, temp, bytesRead, 6, &count);
 			else if (current_size > total * 8 / 10 && current_size <= total * 9 / 10)
-				current_size += this->_readFile(&bufferVector[7], &bufferVector[8], infile, temp, bytes_read, 7, &count);
+				current_size += this->_readFile(&bufferVector[7], &bufferVector[8], infile, temp, bytesRead, 7, &count);
 			else
-				current_size += this->_readFile(&bufferVector[8], &bufferVector[9], infile, temp, bytes_read, 8, &count);
+				current_size += this->_readFile(&bufferVector[8], &bufferVector[9], infile, temp, bytesRead, 8, &count);
 			std::memset(temp, 0, WS_BUFFER_SIZE + 1);
 		}
 	}
@@ -525,7 +537,14 @@ int	EuleeHand::unchunkResponse()
 	int	outfile = open(WS_UNCHUNK_OUTFILE, O_CREAT | O_TRUNC | O_RDWR, 0777);
 	for (size_t i = 0; i < bufferVector.size(); i++)
 	{
-		this->_unchunkIntofile(outfile, bufferVector[i], (i == 0));
+		if (this->_unchunkIntofile(outfile, bufferVector[i], (i == 0)))
+		{
+			close(outfile);
+			std::remove(WS_UNCHUNK_INFILE);
+			std::remove(WS_UNCHUNK_OUTFILE);
+			delete[] temp;
+			return (1);
+		}
 		std::cout << MAGENTA << "Unchunking: " << (i + 1 / bufferVector.size()) + 1 << "0%" << RESET << "\r";
 		std::cout.flush();
 	}
@@ -534,9 +553,9 @@ int	EuleeHand::unchunkResponse()
 
 	infile = open(WS_UNCHUNK_OUTFILE, O_RDONLY, 0777);
 	this->buffer[this->socket].clear();
-	while ((bytes_read = read(infile, temp, WS_BUFFER_SIZE)) > 0)
+	while ((bytesRead = read(infile, temp, WS_BUFFER_SIZE)) > 0)
 	{
-		this->buffer[this->socket].append(temp, bytes_read);
+		this->buffer[this->socket].append(temp, bytesRead);
 		std::memset(temp, 0, WS_BUFFER_SIZE + 1);
 	}
 	close(infile);
@@ -621,26 +640,33 @@ std::string	EuleeHand::extractHTML(std::string path)
 
 int		EuleeHand::sendHttp(int statusCode, std::string responseBody)
 {
-	std::string baseResponse = "HTTP/1.1 " + std::to_string(statusCode) + " " + statusList[statusCode] + " \r\n\r\n";
+	std::string baseResponse = "HTTP/1.1 " + std::to_string(statusCode) + " " + statusList[statusCode] + "\r\n";
+	if (statusCode == 200 && this->cookieExist[this->socket] == false)
+	{
+		std::cout << GREEN << "Sending Cookie..." << RESET << std::endl;
+		this->cookieJar.generateCookie(this->socket);
+		Cookie	cookie = this->cookieJar[this->socket];
+		baseResponse += "Set-Cookie: " + cookie.key + "=" + cookie.value + "; Expires=" + cookie.expireDate + "\r\n";
+	}
+	baseResponse += "\r\n\r\n";
 	if (responseBody.empty() == false)
 	{
 		this->response[this->socket] = baseResponse + responseBody;
 		return (statusCode);
 	}
 
-	if (this->errorpage.find(statusCode) != this->errorpage.end())
+	if (this->errorpage.find(statusCode) != this->errorpage.end() && statusCode != 200)
 	{
 		if (this->checkPath(this->errorpage[statusCode], 1, 0) == 0)
 		{
 			std::cerr << RED << "Error page is not found! Default is used. " << RESET <<std::endl;
 
-			std::string code = "{{error_code}}";
-			std::string msg = "{{error_message}}";
+			std::string codeHolder = "{{error_code}}";
+			std::string msgHolder = "{{error_message}}";
 			baseResponse += extractHTML(WS_ERROR_PAGE_PATH);
-		
-			baseResponse.replace(baseResponse.find(code), code.length(), std::to_string(statusCode));	
-			baseResponse.replace(baseResponse.find(code), code.length(), std::to_string(statusCode));	
-			baseResponse.replace(baseResponse.find(msg), msg.length(), this->statusList[statusCode]);
+			baseResponse.replace(baseResponse.find(codeHolder), codeHolder.length(), std::to_string(statusCode));
+			baseResponse.replace(baseResponse.find(codeHolder), codeHolder.length(), std::to_string(statusCode));	
+			baseResponse.replace(baseResponse.find(msgHolder), msgHolder.length(), this->statusList[statusCode]);
 		}
 		else
 		{
@@ -651,7 +677,16 @@ int		EuleeHand::sendHttp(int statusCode, std::string responseBody)
 	else
 	{
 		if (this->checkPath(this->errorpage[statusCode], 1, 0) == 0)
-			baseResponse += extractHTML(WS_DEFAULT_PAGE_PATH);
+		{
+			if (this->cookieExist[this->socket])
+			{
+				std::string	cookieHolder = "{{cookie}}";
+				baseResponse += extractHTML(WS_COOKIE_PAGE_PATH);
+				baseResponse.replace(baseResponse.find(cookieHolder), cookieHolder.length(), this->cookieJar[this->socket].key + "=" + this->cookieJar[this->socket].value);
+			}
+			else
+				baseResponse += extractHTML(WS_DEFAULT_PAGE_PATH);
+		}
 		else
 			baseResponse += extractHTML(methodPath[this->socket]);
 
@@ -694,29 +729,17 @@ int	EuleeHand::checkClientBodySize()
 size_t	EuleeHand::addEnv(std::string input)
 {
 	size_t	i = 0;
-	std::string	temp = input.substr(0, input.find('='));
-	for (; i < this->_envpSize; ++i)
-	{
-		std::string	str(this->envp[i]);
-		str = str.substr(0, str.find('='));
-		if (str == temp)
-			break ;
-	}
-	if (i == this->_envpSize)
-	{
-		i = 0;
-		this->envp[this->_envpSize] = new char[10000];
-		std::memset(this->envp[this->_envpSize], 0, 10000);
-		for (; input[i]; ++i)
-			this->envp[this->_envpSize][i] = input[i];
-		this->envp[this->_envpSize][i] = '\0';
-		return (++this->_envpSize);
-	}
-	for (size_t j = 0; this->envp[i][j] != '\0'; ++j)
-		this->envp[i][j] = '\0';
-	for (size_t j = 0; input[j]; ++j)
-		this->envp[i][j] = input[j];
-	return (this->_envpSize);
+	for (; input[i]; ++i)
+		this->envp[this->_envpSize][i] = input[i];
+	return (++this->_envpSize);
+}
+
+size_t	EuleeHand::clearEnv()
+{
+	for (size_t i = 0; i < 15; ++i)
+		std::memset(this->envp[i], 0, 1000);
+	this->_envpSize = 0;
+	return (0);
 }
 
 int	EuleeHand::parseHeader()
@@ -734,9 +757,9 @@ int	EuleeHand::parseHeader()
 	size_t	contentLenghtPos = this->buffer[this->socket].find("Content-Length: ");
 	if (contentLenghtPos != std::string::npos)
 	{
-		std::string contentLenghtStr = this->buffer[this->socket].substr(contentLenghtPos + std::strlen("Content-Length: "));
+		std::string contentLenghtStr = this->buffer[this->socket].substr(contentLenghtPos + strlen("Content-Length: "));
 		size_t	contentLenght = std::stoul(contentLenghtStr.substr(0, contentLenghtStr.find("\r\n")));
-		std::string	messageBody = this->buffer[this->socket].substr(headerEndPos + std::strlen("\r\n\r\n"));
+		std::string	messageBody = this->buffer[this->socket].substr(headerEndPos + strlen("\r\n\r\n"));
 		return (messageBody.length() >= (size_t)contentLenght);
 	}
 	return (1);
